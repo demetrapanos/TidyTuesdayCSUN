b4ffa98cf721d6b3088eab2b030555256c4bf831
Demetra <demetrapanos@gmail.com>
2019-08-28 11:23:31 -0700

    Starting tidy tuesday

--- 8-27-19/Day1.R
+++ 8-27-19/Day1.R
@@ -9,4 +9,4 @@ library("tidyverse")
 
 ## read data ##
 simpsons <- readr::read_delim("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-08-27/simpsons-guests.csv", delim = "|", quote = "")
-
+simpsons$count <- 
